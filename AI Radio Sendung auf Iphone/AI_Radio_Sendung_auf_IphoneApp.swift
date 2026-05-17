fb0d751df520fbfe946c958a370df4aead95d5b3
MichaelHein65 <michael.hein.add@gmail.com>
2026-05-17 09:09:29 +0200

    Initial Commit

---
+++ AI Radio Sendung auf Iphone/AI_Radio_Sendung_auf_IphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Radio_Sendung_auf_IphoneApp.swift
+//  AI Radio Sendung auf Iphone
+//
+//  Created by Michael Hein on 17.05.26.
+//
+
+import SwiftUI
+
+@main
+struct AI_Radio_Sendung_auf_IphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Radio Sendung auf Iphone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI Radio Sendung auf Iphone
+//
+//  Created by Michael Hein on 17.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
